b47ebff0679bc3f2cffffe0e5bdd858755b55195
Lapinin Sergey <2@lastrix.org>
2011-08-14 12:08:22 +0400

    Base layout

---
+++ src/org/lastrix/asn1s/Asn1s.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * Copyright (C) 2010-2011 Lastrix                                            *
+ * This file is part of ASN1S.                                                *
+ *                                                                            *
+ * ASN1S is free software: you can redistribute it and/or modify              *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 2 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * ASN1S is distributed in the hope that it will be useful,                   *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the               *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with ASN1S. If not, see <http://www.gnu.org/licenses/>.              *
+ ******************************************************************************/
+
+package org.lastrix.asn1s;
+
+/**
+ * @author: lastrix
+ * Date: 8/14/11
+ * Time: 11:52 AM
+ */
+public class Asn1s {
+}
